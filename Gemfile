be3c7971596c190134fe943d3085937dc76c1977
mford22392 <mford22392@gmail.com>
2015-10-20 16:11:59 -0400

    fix gems again and add db folders and create_users

--- Gemfile
+++ Gemfile
@@ -1,5 +1,12 @@
 source "https://www.rubygems.org"
 
+gem 'sinatra'
+gem 'activerecord', :require => 'active_record'
+gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
+gem 'rake'
+gem 'sqlite3'
+gem 'thin'
+gem 'shotgun'
 gem 'rails'
 gem 'pry'
 gem 'nokogiri'
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,18 @@ Bundler.require
 
 # Require every gem in the gem file.
 
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(
+ :adapter => "sqlite3",
+ :database => "db/songs-#{ENV['SINATRA_ENV']}.db"
+)
+
+ActiveRecord::Base.logger = nil
+
 
 require 'net/https'
 require 'uri'
