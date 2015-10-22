539aa76650c629c37d660a4be551c4850d947001
mford22392 <mford22392@gmail.com>
2015-10-22 07:56:48 -0400

    mistake last time, took stuff out of app.rb i had stuck in there

--- app.rb
+++ app.rb
@@ -4,13 +4,6 @@ class App < Sinatra::Base
 
   def self.run
 
-      alert = Alert.find(5)
-
-      text_input = GoogleDirectionsController.new.build_text(alert)
-
-      Text.new.send_text(text_input)
-      
-
       # Alert.all.each do |alert| 
       #   if alert.text_time == Time.now
       #     GoogleDirectionsController.new.build_text(alert)
